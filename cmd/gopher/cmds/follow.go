2be4746448e8e4736388bde30bcc1259e7cbc095
Masudur Rahman <masudjuly02@gmail.com>
2019-04-30 01:20:56 +0600

    follow users from a file containing a list of users

--- cmd/gopher/cmds/follow.go
+++ cmd/gopher/cmds/follow.go
@@ -1,11 +1,14 @@
 package cmds
 
 import (
+	"bufio"
+	"log"
+	"os"
+
 	"github.com/sanjid133/gopher-love/pkg"
 	_ "github.com/sanjid133/gopher-love/pkg/manager"
 	_ "github.com/sanjid133/gopher-love/pkg/platform"
 	"github.com/spf13/cobra"
-	"log"
 )
 
 func NewCmdFollow() *cobra.Command {
@@ -22,6 +25,25 @@ func NewCmdFollow() *cobra.Command {
 				err = pkg.FollowUser(user)
 				if err != nil {
 					log.Fatalln(err)
+				}
+			} else if file != "" {
+				f, err := os.Open(file)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				defer f.Close()
+
+				reader := bufio.NewReader(f)
+				for {
+					line, isPrefix, err := reader.ReadLine()
+					if err != nil || isPrefix {
+						break
+					}
+
+					err = pkg.FollowUser(string(line))
+					if err != nil {
+						log.Fatalln(err)
+					}
 				}
 			}
 		},
